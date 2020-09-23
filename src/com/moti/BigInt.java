b324cf89e1ef080e051c73e917ddaae2fd147633
Moti Azran <motiazran9@gmail.com>
2020-09-24 00:08:37 +0300

    Add sub, mul and div

--- src/com/moti/BigInt.java
+++ src/com/moti/BigInt.java
@@ -1,8 +1,6 @@
 package com.moti;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 
 public class BigInt implements Comparable{
     // The first index is the units , the second dozens and so on...
@@ -22,14 +20,27 @@ public class BigInt implements Comparable{
             only_digits = number.substring(1);
         }
 
+        _number = new ArrayList<Integer>();
         for (char digit : only_digits.toCharArray()) {
-            _number.add(0, Integer.parseInt(Character.toString(digit)));
+            _number.add(0, Character.getNumericValue(digit));
+        }
+
+        _remove_leading_zores();
+
+        if (_number.get(0) == 0) {
+            _is_plus = true;
         }
     }
 
     private BigInt(ArrayList<Integer> number, boolean is_plus) {
         _number = number;
         _is_plus = is_plus;
+
+        _remove_leading_zores();
+
+        if (_number.get(0) == 0) {
+            _is_plus = true;
+        }
     }
 
     private boolean _valid_number(String number) {
@@ -43,6 +54,64 @@ public class BigInt implements Comparable{
         return index < _number.size() ? _number.get(index) : 0;
     }
 
+    private static int _get_digit(ArrayList<Integer> num, int index) {
+        return index < num.size() ? num.get(index) : 0;
+    }
+
+    private static void _borrow(ArrayList<Integer> num, int index) {
+        int i = 0;
+
+        for (i = index + 1; i < num.size(); ++i) {
+            if (num.get(i) != 0) {
+                num.set(i, num.get(i) - 1);
+                break;
+            }
+        }
+
+        --i;
+        while (i > index) {
+            num.set(i, 9);
+            --i;
+        }
+    }
+
+    private void _remove_leading_zores() {
+        for (int i = _number.size() - 1; i >= 0; --i) {
+            if (_number.get(i) == 0) {
+                _number.remove(i);
+            } else {
+                break;
+            }
+        }
+
+        if (_number.isEmpty()) {
+            _number.add(0);
+            _is_plus = true;
+        }
+    }
+
+    private boolean _is_greater(BigInt rhs) {
+        if (_number.size() > rhs._number.size()) {
+            return true;
+        } else if (_number.size() < rhs._number.size()) {
+            return false;
+        }
+
+        for (int i = _number.size() - 1; i >= 0; --i) {
+            if (_number.get(i) > rhs._number.get(i)) {
+                return true;
+            } else if (_number.get(i) < rhs._number.get(i)) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    private void _swap_sign() {
+        _is_plus = !_is_plus;
+    }
+
     public BigInt plus(BigInt rhs) {
         if (_is_plus && !rhs._is_plus) {
             // x-y
@@ -54,7 +123,11 @@ public class BigInt implements Comparable{
             // -x-y=-(x+y)
             BigInt num1 = new BigInt(_number, true);
             BigInt num2 = new BigInt(rhs._number, true);
-            return new BigInt(num1.plus(num2)._number, false);
+
+            BigInt result = num1.plus(num2);
+            result._swap_sign();
+
+            return result;
         }
 
         // x+y
@@ -65,23 +138,27 @@ public class BigInt implements Comparable{
         for (int i = 0; i < length; ++i) {
             digit_sum += _get_digit(i) + rhs._get_digit(i);
 
-            sum.add(0, digit_sum % 10);
-            digit_sum = (int)Math.floor(digit_sum / 10.0);
+            sum.add(digit_sum % 10);
+            digit_sum = Math.floorDiv(digit_sum, 10);
         }
 
         if (0 != digit_sum) {
-            sum.add(0, 1);
+            sum.add(1);
         }
 
         return new BigInt(sum, true);
     }
 
     public BigInt minus(BigInt rhs) {
-        // -x-y=-(x+y)
         if (!_is_plus && rhs._is_plus) {
+            // -x-y=-(x+y)
             BigInt num1 = new BigInt(_number, true);
             BigInt num2 = new BigInt(rhs._number, true);
-            return new BigInt(num1.minus(num2)._number, false);
+
+            BigInt result = num1.minus(num2);
+            result._swap_sign();
+
+            return result;
         } else if (_is_plus && !rhs._is_plus) {
             // x-(-y)=x+y
             return plus(new BigInt(rhs._number, true));
@@ -89,46 +166,108 @@ public class BigInt implements Comparable{
             // -x-(-y)=y-x
             BigInt num1 = new BigInt(_number, true);
             BigInt num2 = new BigInt(rhs._number, true);
+
             return num2.minus(num1);
         }
 
         // x-y
         int length = Math.max(_number.size(), rhs._number.size());
 
+        ArrayList<Integer> bigger, smaller;
+        boolean is_plus;
+        if (_is_greater(rhs)) {
+            bigger = (ArrayList<Integer>)_number.clone();
+            smaller = (ArrayList<Integer>)rhs._number.clone();
+            is_plus = true;
+        } else {
+            bigger = (ArrayList<Integer>)rhs._number.clone();
+            smaller = (ArrayList<Integer>)_number.clone();
+            is_plus = false;
+        }
+
         ArrayList<Integer> diff = new ArrayList<Integer>();
         int digit_diff = 0;
         for (int i = 0; i < length; ++i) {
-            int subtracting_current_digit = _get_digit(i);
-            int subtracted_current_digit = rhs._get_digit(i);
+            int subtracting_current_digit = _get_digit(bigger, i);
+            int subtracted_current_digit = _get_digit(smaller, i);
 
-            digit_diff += subtracting_current_digit - subtracted_current_digit;
-            if (digit_diff < 0) {
-                diff.add(0, digit_diff + 10);
-                digit_diff = -1;
-            } else {
-                diff.add(0, digit_diff % 10);
-                digit_diff = (int) Math.floor(digit_diff / 10.0);
+            digit_diff = subtracting_current_digit - subtracted_current_digit;
+            if (0 > digit_diff) {
+                _borrow(bigger, i);
+                digit_diff += 10;
             }
+
+            diff.add(digit_diff);
         }
 
-        return new BigInt(diff, digit_diff < 0);
+        return new BigInt(diff, is_plus);
     }
 
     public BigInt multiply(BigInt rhs) {
+        boolean is_plus = _is_plus == rhs._is_plus;
 
+        BigInt product = new BigInt("0");
+        ArrayList<Integer> digit_product = new ArrayList<>();
+        for (int i = 0; i < _number.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                digit_product.add(0);
+            }
+
+            int carry = 0;
+            for (int j = 0; j < rhs._number.size(); ++j) {
+                int prod = (_number.get(i) * rhs._number.get(j)) + carry;
+                digit_product.add(prod % 10);
+                carry = Math.floorDiv(prod, 10);
+            }
+
+            if (carry != 0) {
+                digit_product.add(carry);
+            }
+
+            product = product.plus(new BigInt(digit_product, true));
+            digit_product.clear();
+        }
+
+        product._is_plus = is_plus;
+
+        return product;
     }
 
-    public BigInt divide(BigInt rhs) {
+    public BigInt divide(BigInt rhs) throws ArithmeticException {
+        if (rhs.equals(new BigInt("0")))
+        {
+            throw new ArithmeticException("Zero division");
+        }
 
+        boolean is_plus = _is_plus == rhs._is_plus;
+
+        BigInt quotient = new BigInt("0");
+        BigInt orig_divisor = new BigInt(rhs._number, true);
+        BigInt divisor = new BigInt(rhs._number, true);
+        while (0 >= divisor.compareTo(this)) {
+            quotient = quotient.plus(new BigInt("1"));
+            divisor = divisor.plus(orig_divisor);
+        }
+
+        quotient._is_plus = is_plus;
+
+        return quotient;
     }
 
     @Override
     public String toString() {
-        ArrayList<Character> num = new ArrayList<>();
-        num = (ArrayList<Character>)_number.clone();
-        Collections.reverse(num);
+        String num = new String();
+        if (!_is_plus)
+        {
+            num = num.concat("-");
+        }
 
-        return num.toString();
+        for (int i = _number.size() - 1; i >= 0; --i)
+        {
+            num = num.concat(_number.get(i).toString());
+        }
+
+        return num;
     }
 
     @Override
@@ -145,7 +284,7 @@ public class BigInt implements Comparable{
 
         BigInt num = (BigInt)other;
 
-        return this.minus(num)._number.toString().equals("0");
+        return this.minus(num)._number.get(0) == 0;
     }
 
     @Override
@@ -162,10 +301,10 @@ public class BigInt implements Comparable{
 
         BigInt num = (BigInt)other;
 
-        if (this.minus(num)._number.get(0).equals('-')) {
-            return -1;
+        if (equals(num)) {
+            return 0;
         }
 
-        return this.equals(num) ? 0 : 1;
+        return minus(num)._is_plus ? 1 : -1;
     }
 }
--- src/com/moti/Main.java
+++ src/com/moti/Main.java
@@ -1,8 +1,46 @@
 package com.moti;
 
+import java.util.ArrayList;
+
 public class Main {
 
+    public static void foo(ArrayList<Character> a) {
+        a.add('c');
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        BigInt num1 = new BigInt("123");
+        BigInt num2 = new BigInt("1111");
+        BigInt num3 = new BigInt("9000");
+        BigInt negative_num1 = new BigInt("-123");
+        BigInt negative_num2 = new BigInt("-200");
+
+        System.out.printf("%s - %s = %s\n", num2.toString(), num1.toString(), num2.minus(num1).toString());
+        System.out.printf("%s - %s = %s\n", num1.toString(), num2.toString(), num1.minus(num2).toString());
+
+        System.out.printf("%s - %s = %s\n", num3.toString(), num2.toString(), num3.minus(num2).toString());
+        System.out.printf("%s - %s = %s\n", num2.toString(), num3.toString(), num2.minus(num3).toString());
+
+        System.out.printf("%s + %s = %s\n", num2.toString(), num1.toString(), num2.plus(num1).toString());
+        System.out.printf("%s + %s = %s\n", num1.toString(), num2.toString(), num1.plus(num2).toString());
+
+        System.out.printf("%s + %s = %s\n", num2.toString(), num3.toString(), num2.plus(num3).toString());
+        System.out.printf("%s + %s = %s\n", num3.toString(), num2.toString(), num3.plus(num2).toString());
+
+        System.out.printf("%s + %s = %s\n", negative_num1.toString(), num3.toString(), negative_num1.plus(num3).toString());
+        System.out.printf("%s + %s = %s\n", num3.toString(), negative_num1.toString(), num3.plus(negative_num1).toString());
+
+        System.out.printf("%s * %s = %s\n", num2.toString(), num3.toString(), num2.multiply(num3).toString());
+        System.out.printf("%s * %s = %s\n", num3.toString(), num2.toString(), num3.multiply(num2).toString());
+
+        System.out.printf("%s * %s = %s\n", negative_num1.toString(), num3.toString(), negative_num1.multiply(num3).toString());
+        System.out.printf("%s * %s = %s\n", num3.toString(), negative_num1.toString(), num3.multiply(negative_num1).toString());
+
+        BigInt x = new BigInt("6");
+        BigInt y = new BigInt("3");
+        BigInt z = new BigInt("492");
+
+        System.out.printf("%s / %s = %s\n", x.toString(), y.toString(), x.divide(y).toString());
+        System.out.printf("%s / %s = %s\n", z.toString(), negative_num1.toString(), z.divide(negative_num1).toString());
     }
 }
