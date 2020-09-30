23dec5a81486cfb982fa189fe1f5be1f7c67577e
Moti Azran <motiazran9@gmail.com>
2020-09-30 05:11:39 +0300

    Fix main

--- src/com/moti/Main.java
+++ src/com/moti/Main.java
@@ -1,46 +1,42 @@
 package com.moti;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void foo(ArrayList<Character> a) {
-        a.add('c');
+    public static void main(String[] args) {
+        BigInt num1 = get_number_from_user();
+        BigInt num2 = get_number_from_user();
+
+        System.out.printf("%s + %s = %s\n", num1, num2, num1.plus(num2));
+        System.out.printf("%s - %s = %s\n", num1, num2, num1.minus(num2));
+        System.out.printf("%s * %s = %s\n", num1, num2, num1.multiply(num2));
+
+        try {
+            System.out.printf("%s / %s = %s\n", num1, num2, num1.divide(num2));
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
-    public static void main(String[] args) {
-        BigInt num1 = new BigInt("123");
-        BigInt num2 = new BigInt("1111");
-        BigInt num3 = new BigInt("9000");
-        BigInt negative_num1 = new BigInt("-123");
-        BigInt negative_num2 = new BigInt("-200");
+    private static BigInt get_number_from_user() {
+        Scanner stdin = new Scanner(System.in);
+        BigInt big_num = new BigInt("0");
 
-        System.out.printf("%s - %s = %s\n", num2.toString(), num1.toString(), num2.minus(num1).toString());
-        System.out.printf("%s - %s = %s\n", num1.toString(), num2.toString(), num1.minus(num2).toString());
+        boolean is_number_invalid = false;
+        do {
+            System.out.print("Enter number: ");
+            String num = stdin.nextLine();
+            try {
+                big_num = new BigInt(num);
+                is_number_invalid = false;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+                is_number_invalid = true;
+            }
+        } while(is_number_invalid);
 
-        System.out.printf("%s - %s = %s\n", num3.toString(), num2.toString(), num3.minus(num2).toString());
-        System.out.printf("%s - %s = %s\n", num2.toString(), num3.toString(), num2.minus(num3).toString());
-
-        System.out.printf("%s + %s = %s\n", num2.toString(), num1.toString(), num2.plus(num1).toString());
-        System.out.printf("%s + %s = %s\n", num1.toString(), num2.toString(), num1.plus(num2).toString());
-
-        System.out.printf("%s + %s = %s\n", num2.toString(), num3.toString(), num2.plus(num3).toString());
-        System.out.printf("%s + %s = %s\n", num3.toString(), num2.toString(), num3.plus(num2).toString());
-
-        System.out.printf("%s + %s = %s\n", negative_num1.toString(), num3.toString(), negative_num1.plus(num3).toString());
-        System.out.printf("%s + %s = %s\n", num3.toString(), negative_num1.toString(), num3.plus(negative_num1).toString());
-
-        System.out.printf("%s * %s = %s\n", num2.toString(), num3.toString(), num2.multiply(num3).toString());
-        System.out.printf("%s * %s = %s\n", num3.toString(), num2.toString(), num3.multiply(num2).toString());
-
-        System.out.printf("%s * %s = %s\n", negative_num1.toString(), num3.toString(), negative_num1.multiply(num3).toString());
-        System.out.printf("%s * %s = %s\n", num3.toString(), negative_num1.toString(), num3.multiply(negative_num1).toString());
-
-        BigInt x = new BigInt("6");
-        BigInt y = new BigInt("3");
-        BigInt z = new BigInt("492");
-
-        System.out.printf("%s / %s = %s\n", x.toString(), y.toString(), x.divide(y).toString());
-        System.out.printf("%s / %s = %s\n", z.toString(), negative_num1.toString(), z.divide(negative_num1).toString());
+        return big_num;
     }
 }
